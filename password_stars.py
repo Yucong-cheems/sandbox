78a3c21e474beac603f902a55f06212897fca4e8
YUCONG <yucong.ni@my.jcu.edu.au>
2022-11-23 15:05:09 +0800

    print name

--- password_stars.py
+++ password_stars.py
@@ -1,2 +1,10 @@
-password = float(input("Enter your password: "))
-while password <=
+password = input("Enter your password: ")
+password_length = 8
+while len(password) < password_length:
+    print("password error!")
+    password = input(f"Enter your password with minimum {password_length} characters: ")
+
+if len(password) > password_length:
+    print(len(password) * '*')
+
+
